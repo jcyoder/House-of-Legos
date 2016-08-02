5c3538fcc8a94aec8cd5b3aa77ff6c2000a748cb
jcyoder <kajcyoder@comcast.net>
2016-08-02 16:26:20 -0600

    added jquery functionality to read in Categories JSON file

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* JavaScript */
+
+/*function loadAJAX() {
+	var request;
+	if(window.XMLHttpRequest) {
+		request = new XMLHttpRequest();
+	}
+	else {
+		request = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	request.open('GET', 'categories.json');
+	request.onreadystatechange = function() {
+		if((request.readyState===4) && (request.status===200)) {
+			var items = JSON.parse(request.responseText);
+			console.log(items.Categories);
+			
+			var output = '';		
+			 for (var i=0; i< items.Categories.length; i++) {
+				output += '<a href="#" class="list-group-item category-item">' + items.Categories[i] + '</a>';
+			}
+			console.log(output);
+			document.getElementById('category-items').innerHTML = output;
+		}
+	}
+	request.send();
+}
+
+loadAJAX();
+*/
+
+/*jQuery alternative method*/
+/*$.getJSON('categories.json', function(data) {
+	console.log(data);
+	var output = '';
+	for (var i=0; i< data.Categories.length; i++) {
+				output += '<a href="#" class="list-group-item category-item">' + data.Categories[i] + '</a>';
+	}
+	console.log(output);
+	$('#category-items').html(output);
+});
+*/
+$.getJSON('newcategories.json', function(data) {
+	var output = '';
+	
+	for (var i=0; i< data.Categories.length; i++) {
+		output += '<a href="' + data.Categories[i].URL + '" class="list-group-item category-item">' + data.Categories[i].name + '</a>';
+	}
+
+	$('#category-items').html(output);
+});
