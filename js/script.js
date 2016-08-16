a97ea6b3891719c0718d50f7d04bcb71dfe5f965
jcyoder <kajcyoder@comcast.net>
2016-08-16 16:00:07 -0600

    Now reading from favorite's JSON file for favorites on homepage

--- js/script.js
+++ js/script.js
@@ -46,8 +46,8 @@ $.getJSON('newcategories.json', function(data) {
 	/*outer each handles the Categories object*/
 	$.each(data, function() {
 		$.each(this, function (index, value) {
-			console.log(value.URL + ":" + value.name);
-		output += '<a href="' + value.URL + '" class="list-group-item category-item">' + value.name + '</a>';
+			/*console.log(value.URL + ":" + value.name);*/
+			output += '<a href="' + value.URL + '" class="list-group-item category-item">' + value.name + '</a>';
 		});
 	});
 	
@@ -59,3 +59,33 @@ $.getJSON('newcategories.json', function(data) {
 	$('#category-items').html(output);
 });
 
+$.getJSON('favorites.json', function(data) {
+	
+	var output = '';
+
+	$.each(data, function() {
+		var num = data.Favorites.length;
+		console.log("Length is: " + num);
+		$.each(this, function (index, value) {
+			output += '<section class="section-wrapper">';
+			$.each(this, function (index, value) {
+				console.log(this.length);
+				output += '<h2 class="section-title">' + index +'\'s Favorites</h2>';
+				output += '<div class="row">';
+				
+				$.each(this, function (index, value) {
+					output += '<div class="col-md-4">';
+					output += '<div class="col-box">';
+					output += '<img class="img-responsive" src="' + value.Pic + '" alt="" />';
+					output += '<h3>Category: ' + value.Category + '</h3>';
+					output += '<h3>' + value.Title + '</h3>';
+					output +='</div></div>';
+				});
+			output += '</div>';
+			});
+			output += '</section>';
+		});
+	});
+	console.log(output);
+	$('.favorites').html(output);
+});
